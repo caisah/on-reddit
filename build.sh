5397b70f4d0618d33e706c535e4f7dd4355768b8
caisah <vlad.piersec@protonmail.com>
2017-09-20 21:04:15 +0300

    Fix build & manifest

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 
 rollup --config rollup.config.js;
 
+echo
+echo Copying...
+echo Copying 'manifest.json';
 cp src/manifest.json build/addon/manifest.json;
-cp src/popup/index.html build/addon/index.html;
+echo Copying 'index.html'
+cp src/popup/index.html build/addon/popup.html;
+echo Copying 'style.css'
 cp src/popup/style.css build/addon/style.css;
+echo Copying 'icons'
 cp -R src/icons build/addon/icons;
