3e94e9cc647a83ff2bdc1e594cc0a20651488d39
caisah <vlad.piersec@protonmail.com>
2017-11-18 14:46:32 +0200

    Fix build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+SRC_PATH="./src"
+DEST_PATH="./build/addon"
+
+MANIFEST="manifest.json"
+ICONS="icons"
+STYLES="style.css"
+
+echo running Rollup...
+rollup --config rollup.config.js;
+
+echo
+echo Copying...
+cp "$SRC_PATH/$MANIFEST" "$DEST_PATH/$MANIFEST"
+echo Copied $MANIFEST
+
+cp "$SRC_PATH/popup/index.html" "$DEST_PATH/popup.html"
+echo Copied 'index.html'
+
+cp "$SRC_PATH/popup/$STYLES" "$DEST_PATH/$STYLES"
+echo Copied $STYLES
+
+cp -R "$SRC_PATH/$ICONS" "$DEST_PATH"
+echo Copied $ICONS
---
+++ scripts/pack.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+NAME="on-reddit"
+BUILD_DIR="./build"
+ARCHIVE="$BUILD_DIR/$NAME.zip"
+SRC="$BUILD_DIR/addon"
+
+echo
+echo "Building..."
+
+if [ -e $ARCHIVE ]
+then
+    echo "Deleting old archive..."
+    rm $ARCHIVE
+    echo "Deleted $ARCHIVE"
+fi
+
+echo "Archiving..."
+cd $SRC
+zip -r  "../$NAME.zip" .
+echo "Created $ARCHIVE"
+
